9ecade53b4cbd168eee962fd44320e0d58e821a2
Lucas Berce de Jesus <lucasberce71@gmail.com>
2026-03-15 00:27:05 -0300

    feat: configure eslint and prettier with format on save

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,14 +1,14 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { SignInPage } from '../pages/SignIn'
 import { SignUpPage } from '../pages/SignUp'
 
 export function AppRoutes() {
-    return (
-        <Routes>
-            <Route path='/' element={<Navigate to='/signin' replace />} />
-            <Route path='/signin' element={<SignInPage />} />
-            <Route path='/signup' element={<SignUpPage />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/signin" replace />} />
+      <Route path="/signin" element={<SignInPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
+  )
 }
